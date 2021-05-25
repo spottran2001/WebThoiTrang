a7eb465cdd1edd0f47790fba2c447da732ec2857
JK-NguyenVanDung <77788428+JK-NguyenVanDung@users.noreply.github.com>
2021-05-25 09:16:34 +0700

    Kết nối database, login, logout, quản lý role, thiết kế giao diện navbar

--- WebThoiTrang/Controllers/HomeController.cs
+++ WebThoiTrang/Controllers/HomeController.cs
@@ -12,19 +12,20 @@ namespace WebThoiTrang.Controllers
         {
             return View();
         }
-
+        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
+        [Authorize(Roles = "Admin")]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+        
     }
 }
