3c52ddf613b0363a29cf9f6e04665acacae55c48
Nguyễn Văn Lãm <81850954+Vala227115@users.noreply.github.com>
2021-06-03 21:47:43 +0700

    Quản lý Lô Hàng

---
+++ WebThoiTrang/Areas/Admin/Controllers/LOHANGsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebThoiTrang.Models;
+
+namespace WebThoiTrang.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Nhân Viên")]
+    [Authorize(Roles = "Admin")]
+    public class LOHANGsController : Controller
+    {
+        private CT25Team12Entities db = new CT25Team12Entities();
+
+        // GET: Admin/LOHANGs
+        public ActionResult Index()
+        {
+            var lOHANGs = db.LOHANGs.Include(l => l.NHAPHANKHOI);
+            return View(lOHANGs.ToList());
+        }
+
+        // GET: Admin/LOHANGs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LOHANG lohang = db.LOHANGs.Find(id);
+            if (lohang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lohang);
+        }
+
+        // GET: Admin/LOHANGs/Create
+        public ActionResult Create()
+        {
+            ViewBag.MANPP = new SelectList(db.NHAPHANKHOIs, "MANPP", "QUAN");
+            return View();
+        }
+
+        // POST: Admin/LOHANGs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MALOHANG,NGAYNHAP,MANPP")] LOHANG lohang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.LOHANGs.Add(lohang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MANPP = new SelectList(db.NHAPHANKHOIs, "MANPP", "QUAN", lohang.MANPP);
+            return View(lohang);
+        }
+
+        // GET: Admin/LOHANGs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LOHANG lohang = db.LOHANGs.Find(id);
+            if (lohang == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MANPP = new SelectList(db.NHAPHANKHOIs, "MANPP", "QUAN", lohang.MANPP);
+            return View(lohang);
+        }
+
+        // POST: Admin/LOHANGs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MALOHANG,NGAYNHAP,MANPP")] LOHANG lohang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lohang).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MANPP = new SelectList(db.NHAPHANKHOIs, "MANPP", "QUAN", lohang.MANPP);
+            return View(lohang);
+        }
+
+        // GET: Admin/LOHANGs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LOHANG lohang = db.LOHANGs.Find(id);
+            if (lohang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lohang);
+        }
+
+        // POST: Admin/LOHANGs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            LOHANG lohang = db.LOHANGs.Find(id);
+            db.LOHANGs.Remove(lohang);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
